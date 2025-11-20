df229d71676d0163a331770897dde37e40790d0f
xiften <108333567+not-ani@users.noreply.github.com>
2025-11-20 16:23:55 -0700

    init

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,7 +3,14 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [
+    tsconfigPaths(),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+    nitro(),
+  ],
 });
